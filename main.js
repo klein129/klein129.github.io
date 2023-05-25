d13645b58ec58c9545f6431e2fc31765cae8053d
klein129 <134265949+klein129@users.noreply.github.com>
2023-05-25 22:22:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -66,9 +66,171 @@
 
 
 
-$(document).ready(function () {
-  $("form").submit(handleSubmit);
-});
+// $(document).ready(function () {
+//   $("form").submit(handleSubmit);
+// });
+
+// async function handleSubmit(event) {
+//   event.preventDefault();
+
+//   let data = {
+//     systemEmail: "michal26@michalklein.studenthosting.sk",
+//     contactEmail: $('#email').val(),
+//     message: $('#msg').val(),
+//     // message:""
+//     podmienka: $('#podmienka').val()
+//   };
+
+//     // Jednoduchá podmienka pre zabránenie spamu
+//     if (parseInt(data.podmienka) !== 40) {
+//       // Ak hodnota pola message nie je 40, považujeme to za potenciálny spam
+//       console.log("Spam detected. Form not submitted.");
+//       return;
+//     }
+  
+  
+//   let response = await fetch("https://emailsenderitweek.azurewebsites.net/api/ContactForm", {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   let prevText = $('#sbtn') . text();
+//   if (response.status === 200) {
+//     const res = await response.json();
+//     $('#sbtn').text(res);
+//     await new Promise(r => setTimeout(r, 2500));
+//     $('#sbtn').text(prevText);
+//     if (res === 'Email bol odoslany') {
+//     document.getElementById("contactForm").reset(); // Resetovanie formulára
+//   }}
+//   else {
+//     $('#sbtn').text("Odoslanie zlyhalo");
+//     await new Promise(r => setTimeout(r, 2500));
+//     $('#sbtn').text(prevText);
+//   }
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//   $("form").submit(handleSubmit);
+// });
+
+// async function handleSubmit(event) {
+//   event.preventDefault();
+
+//   let data = {
+//     systemEmail: "michal26@michalklein.studenthosting.sk",
+//     contactEmail: $('#email').val(),
+//     message: $('#msg').val(),
+//     podmienka: $('#podmienka').val()
+//   };
+
+//   // Jednoduchá podmienka pre zabránenie spamu
+//   if (parseInt(data.podmienka) !== 40) {
+//     // Ak hodnota pola podmienka nie je 40, považujeme to za potenciálny spam
+//     console.log("Spam detected. Form not submitted.");
+//     return;
+//   }
+
+//   let response = await fetch("https://emailsenderitweek.azurewebsites.net/api/ContactForm", {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   let prevText = $('#sbtn').text();
+//   if (response.status === 200) {
+//     const res = await response.json();
+//     $('#sbtn').text(res);
+//     await new Promise(r => setTimeout(r, 2500));
+//     $('#sbtn').text(prevText);
+//     if (res === 'Email bol odoslany') {
+//       document.getElementById("contactForm").reset(); // Resetovanie formulára
+//     }
+//   } else {
+//     $('#sbtn').text("Odoslanie zlyhalo");
+//     await new Promise(r => setTimeout(r, 2500));
+//     $('#sbtn').text(prevText);
+//   }
+// }
+
+
+
+
+
+
+// async function handleSubmit(event) {
+//   event.preventDefault();
+
+//   let data = {
+//     systemEmail: "michal26@michalklein.studenthosting.sk",
+//     contactEmail: $('#email').val(),
+//     message: $('#msg').val(),
+//     podmienka: $('#podmienka').val()
+//   };
+
+//   // Jednoduchá podmienka pre zabránenie spamu
+//   if (parseInt(data.podmienka) !== 40) {
+//     // Ak hodnota pola podmienka nie je 40, považujeme to za potenciálny spam
+//     console.log("Spam detected. Form not submitted.");
+//     return;
+//   }
+
+//   let response = await fetch("https://emailsenderitweek.azurewebsites.net/api/ContactForm", {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   let prevText = $('#sbtn').text();
+//   if (response.status === 200) {
+//     const res = await response.json();
+//     $('#sbtn').text(res);
+//     await new Promise(r => setTimeout(r, 2500));
+//     $('#sbtn').text(prevText);
+//     if (res === 'Email bol odoslany') {
+//       document.getElementById("contactForm").reset(); // Resetovanie formulára
+//     }
+//   } else {
+//     $('#sbtn').text("Odoslanie zlyhalo");
+//     await new Promise(r => setTimeout(r, 2500));
+//     $('#sbtn').text(prevText);
+//   }
+
+//   return false; // Zrušiť predvolenú akciu odosielania formulára
+// }
+
+
+
+
+
+
+
+
+
+
 
 async function handleSubmit(event) {
   event.preventDefault();
@@ -77,18 +239,16 @@ async function handleSubmit(event) {
     systemEmail: "michal26@michalklein.studenthosting.sk",
     contactEmail: $('#email').val(),
     message: $('#msg').val(),
-    // message:""
     podmienka: $('#podmienka').val()
   };
 
-    // Jednoduchá podmienka pre zabránenie spamu
-    if (parseInt(data.podmienka) !== 40) {
-      // Ak hodnota pola message nie je 40, považujeme to za potenciálny spam
-      console.log("Spam detected. Form not submitted.");
-      return;
-    }
-  
-  
+  // Jednoduchá podmienka pre zabránenie spamu
+  if (parseInt(data.podmienka) !== 40) {
+    // Ak hodnota pola podmienka nie je 40, považujeme to za potenciálny spam
+    console.log("Spam detected. Form not submitted.");
+    return;
+  }
+
   let response = await fetch("https://emailsenderitweek.azurewebsites.net/api/ContactForm", {
     method: "POST",
     headers: {
@@ -98,23 +258,34 @@ async function handleSubmit(event) {
     body: JSON.stringify(data),
   });
 
-  let prevText = $('#sbtn') . text();
+  let prevText = $('#sbtn').text();
   if (response.status === 200) {
     const res = await response.json();
     $('#sbtn').text(res);
     await new Promise(r => setTimeout(r, 2500));
     $('#sbtn').text(prevText);
-  } else if (res === 'Email bol odoslany'){ 
-    form.reset();
-  }
-  else {
+
+    // Resetovanie formulára po úspešnom odoslaní e-mailu
+    if (res === 'Email bol odoslany') {
+      // Vytvorenie skrytého iframe
+      let iframe = document.createElement('iframe');
+      iframe.style.display = 'none';
+      document.body.appendChild(iframe);
+
+      // Spustenie resetovania formulára po načítaní odpovede z iframe
+      iframe.onload = function () {
+        document.getElementById("contactForm").reset(); // Resetovanie formulára
+        document.body.removeChild(iframe); // Odstránenie iframe
+      };
+
+      // Nastavenie zdroja iframe na prázdnu stránku
+      iframe.src = 'about:blank';
+    }
+  } else {
     $('#sbtn').text("Odoslanie zlyhalo");
     await new Promise(r => setTimeout(r, 2500));
     $('#sbtn').text(prevText);
   }
-  
 }
-
-
 
 
